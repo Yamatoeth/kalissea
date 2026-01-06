653d1d0c010a4829704dc76fe338125da9249be2
Yamato <yamatoeth@users.noreply.github.com>
2026-01-06 15:22:59 +0700

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
   <head>
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="/favicon.ico" />
 
     <title>Kalissea – Agence web spécialisée en développement, SEO et automatisation</title>
 
